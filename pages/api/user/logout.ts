10e134f44fa292696f71f76e82ae6e9cb1837f47
Perry Raskin <perryraskin@gmail.com>
2020-11-03 15:44:10 -0500

    Fixes some type errors

--- pages/api/user/logout.ts
+++ pages/api/user/logout.ts
@@ -4,6 +4,7 @@ import { cookie, decryptCookie } from "../../../utilities/cookie"
 import { serialize } from "cookie"
 
 export default async function(req: NextApiRequest, res: NextApiResponse) {
+  let userFromCookie
   try {
     /* replace current auth cookie with an expired one */
     res.setHeader(
@@ -14,16 +15,14 @@ export default async function(req: NextApiRequest, res: NextApiResponse) {
       })
     )
 
-    let userFromCookie
-
     userFromCookie = await decryptCookie(req.cookies.auth)
+
+    /* log use out of Magic */
+    await magic.users.logoutByToken(userFromCookie.publicAddress)
+
+    return res.json({ authorized: false })
   } catch (error) {
     /* if there's no valid auth cookie, user is not logged in */
     return res.json({ authorized: false, error })
   }
-
-  /* log use out of Magic */
-  await magic.users.logoutByToken(userFromCookie.publicAddress)
-
-  return res.json({ authorized: false })
 }
